f36bbe6aacb8811ba5f0eb5e5426f23772b0156e
Subrata Pramanik <154335629+SUBRATAlsdm@users.noreply.github.com>
2025-12-04 01:21:09 +0530

    db update

--- app/core/database.py
+++ app/core/database.py
@@ -20,6 +20,39 @@
 # Base = declarative_base()
 
 
+# # app/core/database.py
+# import os
+# from dotenv import load_dotenv
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.pool import NullPool
+
+# # load .env in local/dev
+# load_dotenv()
+
+# # Read DB values from single DATABASE_URL or components
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# if not DATABASE_URL:
+#     # fallback to components (if you prefer)
+#     DB_USER = os.getenv("DB_USER", "sql12809023")
+#     DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+#     DB_HOST = os.getenv("DB_HOST", "sql12.freesqldatabase.com")
+#     DB_NAME = os.getenv("DB_NAME", "sql12809023")
+#     DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+# # For serverless, use NullPool and enable pool_pre_ping
+# engine = create_engine(
+#     DATABASE_URL,
+#     echo=False,
+#     pool_pre_ping=True,
+#     poolclass=NullPool
+# )
+
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# Base = declarative_base()
+
+
 # app/core/database.py
 import os
 from dotenv import load_dotenv
@@ -28,25 +61,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-# load .env in local/dev
 load_dotenv()
 
-# Read DB values from single DATABASE_URL or components
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    # fallback to components (if you prefer)
-    DB_USER = os.getenv("DB_USER", "sql12809023")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-    DB_HOST = os.getenv("DB_HOST", "sql12.freesqldatabase.com")
-    DB_NAME = os.getenv("DB_NAME", "sql12809023")
-    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-# For serverless, use NullPool and enable pool_pre_ping
 engine = create_engine(
     DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
-    poolclass=NullPool
+    poolclass=NullPool,
+    connect_args={
+        "ssl": {g
+            "ca": "/var/task/app/core/ca.pem"
+        }
+    },
 )
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
